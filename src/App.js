455e86ead3f309f9f64c2c88a69592cf4586c7ac
Sayantan Samanta <sssamanta789@gmail.com>
2021-09-09 23:06:27 +0530

    Added initial setup

--- src/App.js
+++ src/App.js
@@ -1,115 +1,11 @@
-import './App.css';
-import Navbar from './components/Navbar.component';
-import Auth from './pages/Auth';
-import  { BrowserRouter as Router,Route,Link,Switch, Redirect } from 'react-router-dom';
-import Sidebar from './components/Sidebar.component';
-import styled from 'styled-components';
-import Toasts from './utils/Toasts.component';
-import { useDispatch, useSelector } from 'react-redux';
-import { DangerToast } from './utils/Icons'
-import React from 'react'
-import { TOAST_REMOVER } from './store/action.redux';
-import Dashboard from './pages/Dashboard';
-import MoviePage from './pages/dynamic/MoviePage';
-import { useFirestore, useFirestoreConnect } from 'react-redux-firebase';
-import ButtonComponent from './components/ui/Button.component';
-import db from './db/movie.json';
-import HallBooking from './pages/dynamic/HallBooking';
-import Checkout from './pages/Checkout';
+import { Container } from "@chakra-ui/layout";
 
 function App() {
-
-  useFirestoreConnect([
-    {
-      collection : 'movies',
-    }
-  ])
-
-  const { auth : { authError } } = useSelector(state => state); 
-
-  const state = useSelector(state => state.data?.movies)
-
-  console.log(state);
-
-  const firestore = useFirestore();
-
-  const dispatch = useDispatch();
-
-  const states = useSelector(state => state.firestore);
-
-
-
-  console.log(states);
-  React.useEffect(() => {
-   if(authError){
-    setTimeout(() => {
-      dispatch({ type : TOAST_REMOVER })
-    },3000)
-   }
-  },[authError])
-
-  let dataArray = [];
-
-  for(let i in db){
-    dataArray.push({
-      slug : i,
-      ...db[i]
-    })
-  }
-  console.log(dataArray);
-
-  const postHandler = () => {
-    dataArray.map(data => {
-      return firestore.collection('movies').doc(data.slug).set(data)
-            .then(_ => console.log('Successfully pushed'))
-            .catch(err => console.log(err));
-    })
-  }
   return (
-    <AppContainer>
-      <Sidebar />
-      <DashboardContainer>
-          <Navbar />
-          <ButtonComponent onClick={postHandler}>Googlu</ButtonComponent>
-            <Switch>
-              <Route path="/auth" component={Auth} />
-              <Route path="/" exact component={Dashboard} />
-              <Route path={`/:id`} component={MoviePage} exact/>
-              <Route path={`/:id/:key hallbooking`} component={HallBooking} exact/>
-              <Route path={`/:id/:key hallbooking/checkout`} component={Checkout}/>
-              <Route path="/*login*">
-                <Redirect to="/auth/login" />
-              </Route>
-              <Route path="/*signup*">
-                <Redirect to="/auth/signup" />
-              </Route>
-            </Switch>
-      </DashboardContainer>
-     {
-       authError &&  <Toasts 
-       Icon={DangerToast} 
-       toastText = {authError}
-       color={'rgba(254, 226, 226,1)'}
-       bgColor={'#F24744'}
-     />
-     }
-    </AppContainer>
+   <Container maxW="xl" bg="red.600">
+     shdhsdhs
+   </Container>
   );
 }
 
 export default App;
-
-const AppContainer = styled.section`
-display : flex;
-width: 100vw;
-height: 100vh;
-position: relative;
-`
-
-const DashboardContainer = styled.div`
-flex-basis: 85%;
-padding: 4rem 0rem 2rem 2rem;
-overflow-y:scroll;
-position:sticky;
-overflow-x:hidden;
-`
--- src/index.js
+++ src/index.js
@@ -1,49 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
-import { Provider, useSelector } from 'react-redux';
-import store, { rrfProps } from './store/store.redux';
-import { ReactReduxFirebaseProvider,isLoaded } from 'react-redux-firebase';
-import Spinner from './utils/Spinner.component';
-import styled from 'styled-components';
-//import styled from 'styled-components';
-const LoadingState = styled.div`
-position:fixed;
-top: 0;
-left: 0;
-z-index : 10000;
-height : 100vh;
-width : 100%;
-display: flex;
-justify-content: center;
-align-items: center;
-`
+import { ChakraProvider } from '@chakra-ui/react';
 
-const AuthisLoaded = ({ children }) => {
-   const auth = useSelector((state) => state.firebase.auth);   
-   if(!isLoaded(auth))
-      return (
-       <LoadingState>
-            <Spinner fill="#F46C3F"/>
-       </LoadingState>
-      );   
-   return children;
-}
 
 ReactDOM.render(
  <BrowserRouter>
-    <Provider store={store}>
-     <ReactReduxFirebaseProvider {...rrfProps}>
-        <React.StrictMode>
-         <AuthisLoaded>
-            <App />
-         </AuthisLoaded>
-        </React.StrictMode>
-     </ReactReduxFirebaseProvider>
-    </Provider>
+   <ChakraProvider>
+      <App />
+   </ChakraProvider>
  </BrowserRouter>,
   document.getElementById('root')
 );
